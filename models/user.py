e51be090a6c85304744028b60d2222300884e13f
cxbxtx <cxbxtx@yandex.ru>
2025-11-04 20:09:12 +0300

    edit is_birthdate_near function

--- models/user.py
+++ models/user.py
@@ -1,1 +1,19 @@
 ##здесь будет код
+    def is_birthdate_near(self) -> bool:
+        today = date.today()
+
+        if self.birthdate:
+
+            # Нормализуем дату рождения к текущему году
+            try:
+                birthday_this_year = self.birthdate.date().replace(year=today.year)
+            except ValueError:
+                # Обработка 29 февраля в невисокосный год
+                birthday_this_year = self.birthdate.date().replace(year=today.year, day=28)
+
+            days_until = abs((birthday_this_year - today).days)
+
+            if days_until <= 3:
+                return True
+                
+        return False
