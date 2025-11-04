7a1ef25a4c4d9d553158d6394e73677bbec9fca9
cxbxtx <cxbxtx@yandex.ru>
2025-11-04 23:21:54 +0300

    upd file 1

--- cxbxtx_file_1.py
+++ cxbxtx_file_1.py
@@ -1,33 +1,6 @@
-def add(a, b):
-    """Функция сложения"""
-    return a + b
-
-def subtract(a, b):
-    """Функция вычитания"""
-    return a - b
-
-# Основная программа
-print("Простой калькулятор")
-print("Доступные операции:")
-print("+ - сложение")
-print("- - вычитание") 
-
-# Получаем ввод от пользователя
-try:
-    num1 = float(input("Введите первое число: "))
-    operator = input("Введите оператор (+, -, *, /): ")
-    num2 = float(input("Введите второе число: "))
-    
-    # Выполняем операцию в зависимости от введенного оператора
-    if operator == '+':
-        result = add(num1, num2)
-    elif operator == '-':
-        result = subtract(num1, num2)
-    else:
-        result = "Неверный оператор!"
-    
-    # Выводим результат
-    print(f"Результат: {num1} {operator} {num2} = {result}")
-    
-except ValueError:
-    print("Ошибка: пожалуйста, вводите только числа!")
+# Проверка чётности
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"{num} even number")
+else:
+    print(f"{num} - odd number")
