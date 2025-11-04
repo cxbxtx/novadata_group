f41daaf2d11c6cf25a78daa3df004fe7de9a31e8
cxbxtx <cxbxtx@yandex.ru>
2025-11-04 20:54:43 +0300

    import lib & fix warnings

--- models/user.py
+++ models/user.py
@@ -1,19 +1,21 @@
 ##здесь будет код
-    def is_birthdate_near(self) -> bool:
-        today = date.today()
+from datetime import date
 
-        if self.birthdate:
+def is_birthdate_near(self) -> bool:
+    today = date.today()
+
+    if self.birthdate:
 
             # Нормализуем дату рождения к текущему году
-            try:
-                birthday_this_year = self.birthdate.date().replace(year=today.year)
-            except ValueError:
+        try:
+            birthday_this_year = self.birthdate.date().replace(year=today.year)
+        except ValueError:
                 # Обработка 29 февраля в невисокосный год
-                birthday_this_year = self.birthdate.date().replace(year=today.year, day=28)
+            birthday_this_year = self.birthdate.date().replace(year=today.year, day=28)
 
-            days_until = abs((birthday_this_year - today).days)
+        days_until = abs((birthday_this_year - today).days)
 
-            if days_until <= 3:
-                return True
+        if days_until <= 3:
+            return True
                 
-        return False
+    return False
