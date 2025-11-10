32d8131019c431b358eacfd25fe71ff5f0a7899b
manhu777 <99555631+manhu777@users.noreply.github.com>
2025-11-10 21:35:42 +0300

    add comms to cxbxtx_file_1.py

--- cxbxtx_file_1.py
+++ cxbxtx_file_1.py
@@ -1,6 +1,8 @@
 # Проверка чётности
 num = int(input("Enter a number: "))
+#конструкция if
 if num % 2 == 0:
     print(f"{num} even number")
 else:
+    # выводим значение
     print(f"{num} - odd number")
