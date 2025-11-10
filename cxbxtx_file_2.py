aa63cde91d056c121cb506bc30e985afaf832028
cxbxtx <cxbxtx@yandex.ru>
2025-11-10 21:52:21 +0300

    ch order output

--- cxbxtx_file_2.py
+++ cxbxtx_file_2.py
@@ -1,4 +1,4 @@
 #Конвертер температуры
 celsius = float(input("Enter temperature in Celsius: "))
 fahrenheit = celsius * 9/5 + 32
-print(f"{fahrenheit}°F = {celsius}°C")
+print(f"{celsius}°C = {fahrenheit}°F")
